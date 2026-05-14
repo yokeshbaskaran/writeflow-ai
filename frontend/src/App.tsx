62ca6ff0c4dc7e3d9b698d4b6ef624c90674dfd8
Yokesh <yokeshbaskaran@gmail.com>
2026-05-14 11:05:22 +0530

    routes - login, home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <>
       <div>
-        <Login />
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/auth" element={<Login />} />
+        </Routes>
       </div>
     </>
   );
