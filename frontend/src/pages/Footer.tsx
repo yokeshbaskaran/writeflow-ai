6ce30195009dcb9af7004c3a00ce716f4c549666
Yokesh <yokeshbaskaran@gmail.com>
2026-05-14 11:06:15 +0530

    footer added

--- frontend/src/pages/Footer.tsx
+++ frontend/src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="w-full mx-5 mt-auto pt-2 pb-3 border-t border-gray-100 text-center italic">
+      <div className="w-full mx-5 mt-auto pt-2 pb-3 border-t border-border gradient-primary text-center italic">
         Made with ❤️ by
         <span className="px-2 font-bold not-italic">Yokesh B </span>
       </div>
