559a40dbbb010f6cd5423c6d86a4400a90aad2b3
Yokesh <yokeshbaskaran@gmail.com>
2026-05-20 21:58:21 +0530

    added mobile and desktop screen view

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,12 +3,14 @@ import { CgProfile } from "react-icons/cg";
 import { IoHomeOutline, IoSearch } from "react-icons/io5";
 import { MdLockPerson } from "react-icons/md";
 import { TbBellRinging } from "react-icons/tb";
+import { RiMenuUnfold3Line, RiMenuUnfold4Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import DarkMode from "./DarkMode";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [authUser, setAuthUser] = useState(false);
+  const [authUser] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
 
   const unauthenticatedNav = [
     {
@@ -57,22 +59,47 @@ const Navbar = () => {
     <>
       <section className="h-14 px-3 py-5 border-b border-border flex items-center bg-bg">
         <div className="w-full flex items-center justify-between">
+          {/* Mobile Navbar  */}
+          {
+            <>
+              <div className="md:hidden">
+                <div
+                  onClick={() => setMobileNav(!mobileNav)}
+                  className="p-2 text-white bg-primary rounded"
+                >
+                  {mobileNav ? (
+                    <>
+                      <RiMenuUnfold4Line size={20} />
+                    </>
+                  ) : (
+                    <>
+                      <RiMenuUnfold3Line size={20} />
+                    </>
+                  )}
+                </div>
+              </div>
+            </>
+          }
+
           <div>
             <h2 className="text-xl font-bold text-primary">
               WriteFlow<span className="text-text">.ai</span>
             </h2>
           </div>
 
-          <div className="flex items-center gap-5">
-            {navDetails.map((item, idx) => (
-              <Link
-                key={idx}
-                to={`${item.link}`}
-                className="text-text hove:text-white transition-colors capitalize"
-              >
-                {item.linkName}
-              </Link>
-            ))}
+          {/* Desktop Navbar  */}
+          <div className="max-md:hidden mx-5 w-full flex items-center justify-center">
+            <div className="flex items-center gap-5">
+              {navDetails.map((item, idx) => (
+                <Link
+                  key={idx}
+                  to={`${item.link}`}
+                  className="text-text hove:text-white transition-colors capitalize"
+                >
+                  {item.linkName}
+                </Link>
+              ))}
+            </div>
           </div>
 
           <div>
