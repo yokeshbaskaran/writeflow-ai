ae779775b0b7c70c92d0e406b53458fc810593cb
Yokesh <yokeshbaskaran@gmail.com>
2026-05-21 22:02:44 +0530

    Createpage added

---
+++ frontend/src/pages/Create.tsx
@@ -0,0 +1,29 @@
+import Generate from "./Generate";
+import Response from "./Response";
+
+const Create = () => {
+  const personName = "Yokesh";
+  return (
+    <>
+      <main className="bg-bg">
+        {/* 1. Greetings  */}
+        <header className="py-2 text-center">
+          <h1 className="text-2xl text-text font-bold">
+            Hi! Welcome, {personName}
+          </h1>
+          <p className="text-base text-text-muted">
+            Create amazing content with the power of Al
+          </p>
+        </header>
+
+        {/* 2. Generate content/response  */}
+        <section className="w-full py-5 flex max-md:flex-col justify-center items-stretch gap-7 md:gap-3">
+          <Generate />
+          <Response />
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Create;
