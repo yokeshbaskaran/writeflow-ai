5f2b001b76b3b384d096fb74eea56d7174648ae2
Yokesh <yokeshbaskaran@gmail.com>
2026-05-21 17:45:50 +0530

    Generate page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import MainLayout from "./layouts/MainLayout";
+import Generate from "./pages/Generate";
 
 const App = () => {
   return (
@@ -9,7 +10,12 @@ const App = () => {
       <div>
         {/* Routes  */}
         <Routes>
-          <Route path="/" element={<MainLayout />} />
+          {/* User Route */}
+          <Route path="/" element={<MainLayout />}>
+            <Route path="create" element={<Generate />} />
+          </Route>
+
+          {/* Auth Route */}
           <Route path="/auth" element={<Login />} />
         </Routes>
       </div>
