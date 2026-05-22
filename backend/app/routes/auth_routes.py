fac446a3cfc8d8cb45eab382f089f0dbd0670a84
Yokesh <yokeshbaskaran@gmail.com>
2026-05-22 21:30:30 +0530

    route for signup

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -6,3 +6,23 @@ from db.database import users_collection
 
 # router creating
 router = APIRouter()
+
+
+@router.post("/signup")
+def signup(data: SignupRequest):
+    print("Signup request!")
+
+    # checks existing_user
+    existing_user = users_collection.find_one({"email": data.email})
+
+    # if existing_user already exists returns msg
+    if existing_user:
+        return {"message": "Account Already exists"}
+
+    # creating user
+
+    user = {"name": data.name, "email": data.email, "password": hash_pwd(data.password)}
+
+    users_collection.insert_one(user)
+
+    return {"message": "User created"}
