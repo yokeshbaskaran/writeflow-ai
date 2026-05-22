0653acb136753d7f7dd4767e590583eb0fe5ed04
Yokesh <yokeshbaskaran@gmail.com>
2026-05-22 12:29:09 +0530

    copy button functionality added

--- frontend/src/components/Response.tsx
+++ frontend/src/components/Response.tsx
@@ -1,3 +1,4 @@
+import { BiCopy } from "react-icons/bi";
 const Response = () => {
   return (
     <>
@@ -7,11 +8,38 @@ const Response = () => {
           <h2 className="text-xl text-primary font-semibold">
             AI Generated Response
           </h2>
-
-          <div>copy text icon</div>
         </div>
 
-        <article className="flex-1 h-100 mt-4 px-2 py-3 text-sm border border-border-strong rounded md:max-h-100 overflow-y-auto scrollbar-thin"></article>
+        <div className="relative">
+          <article className="flex-1 h-100 mt-4 px-3 py-5 text-sm text-justify border border-border-strong rounded md:max-h-100 overflow-y-auto scrollbar-thin">
+            {/* Dummy text  */}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            modi, eius explicabo itaque eos deleniti amet quia dolor sunt
+            adipisci illo, veniam corporis suscipit tempora nam commodi,
+            reiciendis perspiciatis qui. Distinctio magni exercitationem
+            consequatur repellat commodi officia, libero, praesentium, quisquam
+            rerum itaque quod impedit debitis quaerat earum dolor iusto in et
+            labore aspernatur reprehenderit sequi non eos. Asperiores, ipsa
+            rerum? Blanditiis, beatae minima iusto architecto officia
+            perferendis. Molestias sed ipsum officia nobis harum distinctio eius
+            totam saepe. Corrupti blanditiis iusto, nobis, aperiam voluptatem
+            nulla, rem beatae ad ipsum saepe nesciunt. Sit rem magnam nisi
+            asperiores maiores hic ad quas amet expedita vel, quibusdam nemo
+            quis excepturi mollitia temporibus, voluptatum ipsam velit. Aliquid
+            ad culpa ipsam ullam asperiores? Alias nostrum ipsa numquam corrupti
+            vel error quisquam deserunt quas a, incidunt nobis. Liba ipsam ullam
+            asperiores? Alias nostrum ipsa numquam corrupti vel error quisquam
+            deserunt quas a, incidunt nobis. Libero deserunt et animi tempora
+            labore! Debitis quis, optio a ut alias consequatur recusandae quos
+            necessitatibus ratione?
+          </article>
+
+          <div className="mt-6 mx-5 absolute top-0 right-0">
+            <button className="p-1 bg-bg text-primary border border-border rounded hover:text-white hover:bg-primary cursor-pointer">
+              <BiCopy size={23} />
+            </button>
+          </div>
+        </div>
 
         <div className="text-right">
           <span className="text-xs text-text-muted">600 words</span>
