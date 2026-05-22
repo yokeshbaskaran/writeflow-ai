f07ecfa1177d2343ed92e266e40b538852324a9f
Yokesh <yokeshbaskaran@gmail.com>
2026-05-22 22:26:15 +0530

    added "CORS"

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,24 @@ from fastapi import FastAPI
 from fastapi import Depends
 from routes.auth_routes import router as auth_router
 from dependencies.auth_dependency import get_current_user
+from fastapi.middleware.cors import CORSMiddleware
 
 # creating app
 app = FastAPI()
+
+# CORS
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # add/include the routes from other folders
 app.include_router(auth_router)
