26a29d961f80f3298ad8e06d7013039a655d6a46
Yokesh <yokeshbaskaran@gmail.com>
2026-05-22 21:32:04 +0530

    Schema for login and signup

---
+++ backend/app/schemas/auth_schema.py
@@ -0,0 +1,13 @@
+# Create User Schema
+from pydantic import BaseModel, EmailStr
+
+
+class SignupRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
