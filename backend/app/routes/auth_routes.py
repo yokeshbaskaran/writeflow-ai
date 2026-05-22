483952e347a9850cf10ffb7ad559daf5fb3f7943
Yokesh <yokeshbaskaran@gmail.com>
2026-05-22 21:30:45 +0530

    route for login

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -8,6 +8,23 @@ from db.database import users_collection
 router = APIRouter()
 
 
+@router.post("/login")
+def login(data: LoginRequest):
+    print("Login request!")
+
+    findUser = users_collection.find_one({"email": data.email})
+
+    if not findUser:
+        return {"message": "Invalid credentials"}
+
+    is_valid_pwsd = verify_pwd(data.password, findUser["password"])
+
+    if not is_valid_pwsd:
+        return {"message": "Invalid credential"}
+
+    return {"msg": "User logined!"}
+
+
 @router.post("/signup")
 def signup(data: SignupRequest):
     print("Signup request!")
