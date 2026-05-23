4cebd28da32e32d99aefa6f6c6a95d10ae938439
Yokesh <yokeshbaskaran@gmail.com>
2026-05-23 12:25:42 +0530

    authorization devps

--- backend/app/dependencies/auth_dependency.py
+++ backend/app/dependencies/auth_dependency.py
@@ -1,13 +1,21 @@
-from jose import jwt
+from jose import jwt, JWTError
 from fastapi import Header
 from config import SECRET_KEY, ALGORITHM
+from fastapi import HTTPException
 
 
+# checks current user is authorized
 def get_current_user(authorization: str = Header(...)):
-    token = authorization.replace("Bearer ", "")
 
-    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
-    # print("payload:::", payload)
+    try:
+        token = authorization.replace("Bearer ", "")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
 
-    return payload
+        # print("payload:::", payload)
+        return payload
+    #
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
