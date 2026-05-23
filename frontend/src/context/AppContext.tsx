e3c20199c6c1cadcc1be3833b421fdaa6159c9e6
Yokesh <yokeshbaskaran@gmail.com>
2026-05-23 18:54:25 +0530

    removed dummy auth

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -1,32 +1,21 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 //types
-type AuthStateType = {
-  email: string;
-  password: string;
-};
-
-// type ResponseSectionType = {
-//   heading: string;
-//   content: string;
+// type AuthStateType = {
+//   email: string;
+//   password: string;
 // };
 
-// type AiResponseType = {
-//   title: string;
-//   introduction: string;
-//   sections: ResponseSectionType[];
-//   conclusion: string;
-// };
-
-// export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = import.meta.env.VITE_API_URL;
+// console.log("API_URL", API_URL);
 
 type AppContextType = {
   pathToHome: () => void;
-  login: (email: string, password: string) => void;
-  logout: () => void;
-  authUser: AuthStateType | null;
-  setAuthUser: (user: AuthStateType | null) => void;
+  // login: (email: string, password: string) => void;
+  // logout: () => void;
+  // authUser: AuthStateType | null;
+  // setAuthUser: (user: AuthStateType | null) => void;
 };
 
 const AppContext = createContext({} as AppContextType);
@@ -42,28 +31,13 @@ type AppContextProviderType = {
 
 export const AppContextProvider = ({ children }: AppContextProviderType) => {
   // dummy authentication
-  const [authUser, setAuthUser] = useState<AuthStateType | null>(null);
 
   //when page reloads
   useEffect(() => {
-    const stored = localStorage.getItem("userdata");
-
-    if (!stored) return;
-    setAuthUser(JSON.parse(stored));
+    // const stored = localStorage.getItem("userdata");
+    // if (!stored) return;
+    // setAuthUser(JSON.parse(stored));
   }, []);
-
-  // dummy User login
-  const login = (email: string, password: string) => {
-    const authData = { email, password };
-
-    setAuthUser(authData);
-    localStorage.setItem("userdata", JSON.stringify(authData));
-  };
-
-  // dummy User Logout
-  const logout = () => {
-    setAuthUser(null);
-  };
 
   // Redirects to Homepage
   const navigate = useNavigate();
@@ -74,11 +48,6 @@ export const AppContextProvider = ({ children }: AppContextProviderType) => {
   //context values
   const contextValue = {
     pathToHome,
-    // auth
-    login,
-    logout,
-    authUser,
-    setAuthUser,
   };
 
   return (
