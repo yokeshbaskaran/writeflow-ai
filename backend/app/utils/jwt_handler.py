eac6178d2e25f596ae4960f3176c71c126da0914
Yokesh <yokeshbaskaran@gmail.com>
2026-05-24 22:56:11 +0530

    .

--- backend/app/utils/jwt_handler.py
+++ backend/app/utils/jwt_handler.py
@@ -4,7 +4,7 @@ from config import SECRET_KEY, ALGORITHM
 
 
 def create_token(email: str):
-    payload = {"sub": email, "exp": datetime.utcnow() + timedelta(minutes=30)}
+    payload = {"sub": email, "exp": datetime.utcnow() + timedelta(hours=1)}
     # print("payload:::", payload)
 
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
