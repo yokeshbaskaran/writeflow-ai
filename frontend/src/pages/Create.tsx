7da58cccaf8acc10452ddc389f7f2a1b09a9c610
Yokesh <yokeshbaskaran@gmail.com>
2026-05-27 19:34:30 +0530

    Create page removed authuser

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,23 +1,15 @@
 import Generate from "../components/Generate";
 import Response from "../components/Response";
-import { useAppContext } from "../context/AppContext";
 
 const Create = () => {
-  const { authUser } = useAppContext();
-  // username
-  const personName = authUser?.username || "Guest";
-
   return (
     <>
       <main className="bg-bg">
         {/* 1. Greetings  */}
         <header className="py-2 text-center">
-          <h1 className="text-2xl text-text font-bold">
-            Hi! Welcome,
-            <span className="pl-1 capitalize">{authUser && personName}</span>
-          </h1>
+          <h1 className="text-2xl text-text font-bold">Generate New Content</h1>
           <p className="text-base text-text-muted">
-            Create amazing content with the power of Al
+            Create amazing content with the power of AI in seconds.
           </p>
         </header>
 
