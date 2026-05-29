765a9c7228cdbce05affeab291d7e734e5564a5c
Yokesh <yokeshbaskaran@gmail.com>
2026-05-29 16:34:26 +0530

    metrics data changed

--- frontend/src/components/Metrics.tsx
+++ frontend/src/components/Metrics.tsx
@@ -29,64 +29,48 @@ const Metrics = () => {
     icon: <HiOutlineSparkles size={30} />,
     title: "Credits left",
     data: creditsData,
+    bgColor: "bg-green-700",
   };
 
   const words = {
     icon: <IoDocumentTextSharp size={30} />,
     title: "Words generated",
     data: wordsData,
+    bgColor: "bg-blue-700",
   };
 
   const content = {
     icon: <HiOutlinePencilAlt size={30} />,
     title: "Contents created",
     data: contentData,
+    bgColor: "bg-primary",
   };
+
+  const metricsList = [credits, words, content];
 
   return (
     <>
-      <section className="my-3 px-2 flex max-md:flex-col items-center justify-between gap-5">
+      <section className="my-3 px-2 grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-7">
         {/* Credits left  */}
-        <div className="w-full px-4 py-3 flex items-center gap-3 border border-border-strong rounded">
-          <div className="p-2 text-white bg-violet-700 border rounded-xl">
-            {credits.icon}
-          </div>
+        {metricsList.map((item, idx) => (
+          <div
+            key={idx}
+            className="w-full px-4 py-3 flex items-center gap-3 bg-bg-soft border border-border-strong rounded-xl"
+          >
+            <div className={`p-2 text-white border rounded-xl ${item.bgColor}`}>
+              {item.icon}
+            </div>
 
-          <div className="flex flex-col items-start gap-1">
-            <h2 className="text-sm text-text-muted font-semibold">
-              {credits.title}
-            </h2>
-            <span className="text-text text-xl font-bold">{credits.data}</span>
+            <div className="flex flex-col items-start gap-1">
+              <h2 className="text-sm text-text-muted font-semibold">
+                {item.title}
+              </h2>
+              <span className="text-text text-xl font-bold">{item.data}</span>
+            </div>
           </div>
-        </div>
-
-        {/* Words generated  */}
-        <div className="w-full px-4 py-3 flex items-center gap-3 border border-border-strong rounded">
-          <div className="p-2 text-white bg-blue-700 border rounded-xl">
-            {words.icon}
-          </div>
-
-          <div className="flex flex-col items-start gap-1">
-            <h2 className="text-sm text-text-muted font-semibold">
-              {words.title}
-            </h2>
-            <span className="text-text text-xl font-bold">{words.data}</span>
-          </div>
-        </div>
+        ))}
 
         {/* Words generated  */}
-        <div className="w-full px-4 py-3 flex items-center gap-3 border border-border-strong rounded">
-          <div className="p-2 text-white bg-green-700 border rounded-xl">
-            {content.icon}
-          </div>
-
-          <div className="flex flex-col items-start gap-1">
-            <h2 className="text-sm text-text-muted font-semibold">
-              {content.title}
-            </h2>
-            <span className="text-text text-xl font-bold">{content.data}</span>
-          </div>
-        </div>
       </section>
     </>
   );
