1eafe0c6bf6a2f68028b0a8a2abc4bd1e2a00e21
Yokesh <yokeshbaskaran@gmail.com>
2026-05-29 16:34:58 +0530

    Dashboard layout

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { DoughnutChart, LineChart } from "../components/Chart";
 import History from "../components/History";
 import Metrics from "../components/Metrics";
 import { useAppContext } from "../context/AppContext";
@@ -7,44 +8,61 @@ const Dashboard = () => {
   const personName = authUser?.username;
 
   return (
-    <main className="flex flex-col overflow-hidden">
-      {/* DashBoard */}
-      <section className="shrink-0">
-        <div className="my-3 mx-3 text-center">
-          <h1 className="text-2xl font-bold">
-            Hi! Welcome,{" "}
-            <span className="text-primary capitalize">{personName}</span>
-          </h1>
+    <>
+      <main className="px-2 flex max-md:flex-col gap-2 md:overflow-hidden">
+        {/* Left Section */}
+        <section className="shrink-0 md:w-7/12">
+          <div className="flex flex-col gap-2">
+            <div className="my-2 mx-3 text-left">
+              <h1 className="text-2xl font-bold">
+                Hi! Welcome,{" "}
+                <span className="text-primary capitalize">{personName}</span>
+              </h1>
 
-          <p className="mt-1 text-text-muted">
-            Here's what's happening with your content today.
-          </p>
-        </div>
-      </section>
+              <p className="mt-1 text-sm text-text-muted">
+                Here's what's happening with your content today.
+              </p>
+            </div>
 
-      {/* Metrics Data Section */}
-      <section>
-        <Metrics />
-      </section>
+            <div>
+              {/* Metrics Data Section */}
+              <Metrics />
+            </div>
 
-      {/* History Section  */}
-      <section className="min-h-0 flex flex-1 flex-col px-3 pb-4">
-        <div className="px-3 shrink-0">
-          <h2 className="text-xl font-bold">History Responses:</h2>
-          <p className="my-1 text-text-muted text-base">
-            All your generated content in one place.
-          </p>
-        </div>
+            {/* Charts */}
+            <section className="p-2 grid grid-cols-1 xl:grid-cols-3 gap-3">
+              {/* Usage Overview */}
+              <div className="xl:col-span-2 rounded-2xl border border-border bg-bg-soft p-4">
+                <LineChart />
+              </div>
 
-        {/* Divider  */}
-        <div className="w-full my-2 border-b border-border"></div>
+              {/* Content Type */}
+              <div className="rounded-2xl border border-border bg-bg-soft p-3">
+                <DoughnutChart />
+              </div>
+            </section>
+          </div>
+        </section>
 
-        <div className="min-h-0 flex-1 overflow-y-auto scrollbar-none pr-2">
-          {response && <History />}
-          {response.length === 0 && <span>No Responses Generated</span>}
-        </div>
-      </section>
-    </main>
+        {/* Right Section - History */}
+        <section className="md:w-5/12 min-h-0 flex flex-1 flex-col px-3 py-2 pb-2">
+          <div className="px-2 shrink-0">
+            <h2 className="text-xl font-bold">History Responses:</h2>
+            <p className="my-1 text-text-muted text-base">
+              All your generated content in one place.
+            </p>
+          </div>
+
+          {/* Divider  */}
+          <div className="w-full my-2 border-b border-border"></div>
+
+          <div className="min-h-0 flex-1 overflow-y-auto scrollbar-none">
+            {response && <History />}
+            {response.length === 0 && <span>No Responses Generated</span>}
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
